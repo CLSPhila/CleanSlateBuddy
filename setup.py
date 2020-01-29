0a16f46bcffd4e08b367f5da4a3e6b82e293f4e1
Nate Vogel <nathan.c.vogel@gmail.com>
2020-01-29 10:46:14 -0500

    renamed package to lowercase cleanslatebuddy

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
                 out.setdefault(package, []).append(prefix+name)
     return out
 
-setup(name='docassemble.CleanSlateBuddy',
+setup(name='docassemble.cleanslatebuddy',
       version='1.0',
       description=('Get help preparing expungement petitions'),
       long_description=u'# Clean Slate Buddy\r\n\r\nSkip the _really_ tedious parts of filling out Expungement petitions. \r\n\r\nUse this Docassemble guided interview to automatically generate petitions\r\nfor expungements and sealings.\r\n\r\nYou still have to enter the records you want to expunge by hand, but this interview\r\nwill fill in the required petitions for you. \r\n\r\n',
@@ -55,6 +55,6 @@ setup(name='docassemble.CleanSlateBuddy',
       namespace_packages=['docassemble'],
       install_requires=[],
       zip_safe=False,
-      package_data=find_package_data(where='docassemble/CleanSlateBuddy/', package='docassemble.CleanSlateBuddy'),
+      package_data=find_package_data(where='docassemble/cleanslatebuddy/', package='docassemble.cleanslatebuddy'),
      )
 
